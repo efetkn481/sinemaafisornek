cae2c4575a11bde6bbfa60ea04d5f79e846127a9
efetekin481@outlook.com <efetekin481@outlook.com>
2023-03-21 23:43:56 +0300

    Add project files.

---
+++ sinemafragmanornek/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace sinemafragmanornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kategorilerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aksiyonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.romantikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bilimKurguToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.komediToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.johnWickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.matrixToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jumpStreetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.renklerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.maviToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turuncuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.griToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hakkımızdaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Pcteternalsunshine = new System.Windows.Forms.PictureBox();
+            this.PctJohnWick = new System.Windows.Forms.PictureBox();
+            this.PctJumpStreet = new System.Windows.Forms.PictureBox();
+            this.PctMatrix = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pcteternalsunshine)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctJohnWick)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctJumpStreet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kategorilerToolStripMenuItem,
+            this.renklerToolStripMenuItem,
+            this.hakkToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kategorilerToolStripMenuItem
+            // 
+            this.kategorilerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aksiyonToolStripMenuItem,
+            this.romantikToolStripMenuItem,
+            this.bilimKurguToolStripMenuItem,
+            this.komediToolStripMenuItem});
+            this.kategorilerToolStripMenuItem.Name = "kategorilerToolStripMenuItem";
+            this.kategorilerToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.kategorilerToolStripMenuItem.Text = "Kategoriler";
+            // 
+            // aksiyonToolStripMenuItem
+            // 
+            this.aksiyonToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.johnWickToolStripMenuItem});
+            this.aksiyonToolStripMenuItem.Name = "aksiyonToolStripMenuItem";
+            this.aksiyonToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.aksiyonToolStripMenuItem.Text = "Aksiyon";
+            // 
+            // romantikToolStripMenuItem
+            // 
+            this.romantikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem});
+            this.romantikToolStripMenuItem.Name = "romantikToolStripMenuItem";
+            this.romantikToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.romantikToolStripMenuItem.Text = "Romantik";
+            // 
+            // bilimKurguToolStripMenuItem
+            // 
+            this.bilimKurguToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.matrixToolStripMenuItem});
+            this.bilimKurguToolStripMenuItem.Name = "bilimKurguToolStripMenuItem";
+            this.bilimKurguToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.bilimKurguToolStripMenuItem.Text = "Bilim Kurgu";
+            // 
+            // komediToolStripMenuItem
+            // 
+            this.komediToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jumpStreetToolStripMenuItem});
+            this.komediToolStripMenuItem.Name = "komediToolStripMenuItem";
+            this.komediToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.komediToolStripMenuItem.Text = "Komedi";
+            // 
+            // johnWickToolStripMenuItem
+            // 
+            this.johnWickToolStripMenuItem.Name = "johnWickToolStripMenuItem";
+            this.johnWickToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.johnWickToolStripMenuItem.Text = "John Wick";
+            this.johnWickToolStripMenuItem.Click += new System.EventHandler(this.johnWickToolStripMenuItem_Click);
+            // 
+            // eternalSunshineOfTheSpotlessMindToolStripMenuItem
+            // 
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem.Name = "eternalSunshineOfTheSpotlessMindToolStripMenuItem";
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem.Size = new System.Drawing.Size(340, 26);
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem.Text = "Eternal Sunshine of the Spotless Mind";
+            this.eternalSunshineOfTheSpotlessMindToolStripMenuItem.Click += new System.EventHandler(this.eternalSunshineOfTheSpotlessMindToolStripMenuItem_Click);
+            // 
+            // matrixToolStripMenuItem
+            // 
+            this.matrixToolStripMenuItem.Name = "matrixToolStripMenuItem";
+            this.matrixToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.matrixToolStripMenuItem.Text = "Matrix";
+            this.matrixToolStripMenuItem.Click += new System.EventHandler(this.matrixToolStripMenuItem_Click);
+            // 
+            // jumpStreetToolStripMenuItem
+            // 
+            this.jumpStreetToolStripMenuItem.Name = "jumpStreetToolStripMenuItem";
+            this.jumpStreetToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.jumpStreetToolStripMenuItem.Text = "Jump Street";
+            this.jumpStreetToolStripMenuItem.Click += new System.EventHandler(this.jumpStreetToolStripMenuItem_Click);
+            // 
+            // renklerToolStripMenuItem
+            // 
+            this.renklerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maviToolStripMenuItem,
+            this.turuncuToolStripMenuItem,
+            this.griToolStripMenuItem});
+            this.renklerToolStripMenuItem.Name = "renklerToolStripMenuItem";
+            this.renklerToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.renklerToolStripMenuItem.Text = "Renkler";
+            // 
+            // maviToolStripMenuItem
+            // 
+            this.maviToolStripMenuItem.Name = "maviToolStripMenuItem";
+            this.maviToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.maviToolStripMenuItem.Text = "Mavi";
+            this.maviToolStripMenuItem.Click += new System.EventHandler(this.maviToolStripMenuItem_Click);
+            // 
+            // turuncuToolStripMenuItem
+            // 
+            this.turuncuToolStripMenuItem.Name = "turuncuToolStripMenuItem";
+            this.turuncuToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.turuncuToolStripMenuItem.Text = "Turuncu";
+            this.turuncuToolStripMenuItem.Click += new System.EventHandler(this.turuncuToolStripMenuItem_Click);
+            // 
+            // griToolStripMenuItem
+            // 
+            this.griToolStripMenuItem.Name = "griToolStripMenuItem";
+            this.griToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.griToolStripMenuItem.Text = "Gri";
+            this.griToolStripMenuItem.Click += new System.EventHandler(this.griToolStripMenuItem_Click);
+            // 
+            // hakkToolStripMenuItem
+            // 
+            this.hakkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hakkımızdaToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.hakkToolStripMenuItem.Name = "hakkToolStripMenuItem";
+            this.hakkToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.hakkToolStripMenuItem.Text = "Hızlı Erişim";
+            // 
+            // hakkımızdaToolStripMenuItem
+            // 
+            this.hakkımızdaToolStripMenuItem.Name = "hakkımızdaToolStripMenuItem";
+            this.hakkımızdaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.hakkımızdaToolStripMenuItem.Text = "Hakkımızda";
+            this.hakkımızdaToolStripMenuItem.Click += new System.EventHandler(this.hakkımızdaToolStripMenuItem_Click);
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            this.çıkışToolStripMenuItem.Click += new System.EventHandler(this.çıkışToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // Pcteternalsunshine
+            // 
+            this.Pcteternalsunshine.Image = ((System.Drawing.Image)(resources.GetObject("Pcteternalsunshine.Image")));
+            this.Pcteternalsunshine.Location = new System.Drawing.Point(12, 68);
+            this.Pcteternalsunshine.Name = "Pcteternalsunshine";
+            this.Pcteternalsunshine.Size = new System.Drawing.Size(776, 370);
+            this.Pcteternalsunshine.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Pcteternalsunshine.TabIndex = 3;
+            this.Pcteternalsunshine.TabStop = false;
+            this.Pcteternalsunshine.Visible = false;
+            // 
+            // PctJohnWick
+            // 
+            this.PctJohnWick.Image = ((System.Drawing.Image)(resources.GetObject("PctJohnWick.Image")));
+            this.PctJohnWick.Location = new System.Drawing.Point(12, 68);
+            this.PctJohnWick.Name = "PctJohnWick";
+            this.PctJohnWick.Size = new System.Drawing.Size(776, 370);
+            this.PctJohnWick.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PctJohnWick.TabIndex = 4;
+            this.PctJohnWick.TabStop = false;
+            this.PctJohnWick.Visible = false;
+            // 
+            // PctJumpStreet
+            // 
+            this.PctJumpStreet.Image = ((System.Drawing.Image)(resources.GetObject("PctJumpStreet.Image")));
+            this.PctJumpStreet.Location = new System.Drawing.Point(12, 68);
+            this.PctJumpStreet.Name = "PctJumpStreet";
+            this.PctJumpStreet.Size = new System.Drawing.Size(776, 370);
+            this.PctJumpStreet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PctJumpStreet.TabIndex = 5;
+            this.PctJumpStreet.TabStop = false;
+            this.PctJumpStreet.Visible = false;
+            // 
+            // PctMatrix
+            // 
+            this.PctMatrix.Image = ((System.Drawing.Image)(resources.GetObject("PctMatrix.Image")));
+            this.PctMatrix.Location = new System.Drawing.Point(12, 68);
+            this.PctMatrix.Name = "PctMatrix";
+            this.PctMatrix.Size = new System.Drawing.Size(776, 370);
+            this.PctMatrix.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PctMatrix.TabIndex = 6;
+            this.PctMatrix.TabStop = false;
+            this.PctMatrix.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PctMatrix);
+            this.Controls.Add(this.PctJumpStreet);
+            this.Controls.Add(this.PctJohnWick);
+            this.Controls.Add(this.Pcteternalsunshine);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pcteternalsunshine)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctJohnWick)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctJumpStreet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PctMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kategorilerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aksiyonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem johnWickToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem romantikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eternalSunshineOfTheSpotlessMindToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bilimKurguToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem matrixToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem komediToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jumpStreetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem renklerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem maviToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turuncuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem griToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hakkımızdaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Pcteternalsunshine;
+        private System.Windows.Forms.PictureBox PctJohnWick;
+        private System.Windows.Forms.PictureBox PctJumpStreet;
+        private System.Windows.Forms.PictureBox PctMatrix;
+    }
+}
+
---
+++ sinemafragmanornek/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinemafragmanornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maviToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void turuncuToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void griToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void hakkımızdaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bu sayfa Efecan Tekin'e aittir.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void çıkışToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void johnWickToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Pcteternalsunshine.Visible = false;
+            PctMatrix.Visible = false;
+            PctJumpStreet.Visible = false;
+            PctJohnWick.Visible = true;
+            label1.Visible = true;
+            label1.Text = "John Wick";
+        }
+
+        private void eternalSunshineOfTheSpotlessMindToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PctJumpStreet.Visible = false;
+            PctJohnWick.Visible = false;
+            PctMatrix.Visible = false;
+            Pcteternalsunshine.Visible = true;
+            label1.Visible = true;
+            label1.Text = "Eternal Sunshine of the Spotless Mind";
+        }
+
+        private void matrixToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PctJumpStreet.Visible = false;
+            PctJohnWick.Visible = false;
+            Pcteternalsunshine.Visible = false;
+            PctMatrix.Visible= true;
+            label1.Visible = true;
+            label1.Text = "Matrix";
+        }
+
+        private void jumpStreetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            PctMatrix.Visible = false;
+            PctJohnWick.Visible = false;
+            Pcteternalsunshine.Visible = false;
+            PctJumpStreet.Visible = true;
+            label1.Visible = true;
+            label1.Text = "Jump Street";
+        }
+    }
+}
